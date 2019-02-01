6d12cc6fa7c760de39bfe1692a8b88455a2490d9
rong <rzha419@aucklanduni.ac.nz>
2019-02-01 16:51:35 +1300

    TwoNode Operator verification

--- src/consoperators/TwoNodeOperator.java
+++ src/consoperators/TwoNodeOperator.java
@@ -22,19 +22,18 @@ public class TwoNodeOperator extends TreeOperator {
     RealParameter rates;
     double twindowSize;
     BranchRateModel.Base branchRateModel;
+    Tree tree;
 
     @Override
     public void initAndValidate() {
         rates = rateInput.get();
         twindowSize = twindowSizeInput.get();
         branchRateModel = branchRateModelInput.get();
-
+        tree = treeInput.get(this);
     }
 
     @Override
     public double proposal() {
-        //the tree to work with
-        Tree tree = treeInput.get(this);
         //the internal node to operate on
         Node node;
         //hastings ratio to return
@@ -44,9 +43,8 @@ public class TwoNodeOperator extends TreeOperator {
         Step1: randomly select one internal node in the tree
          */
         int nodeCount = tree.getNodeCount();
-        //tree.getInternalNodes()
         do {
-            final int nodeNr = nodeCount / 2 + 1 + Randomizer.nextInt(nodeCount / 2);
+            int nodeNr = nodeCount / 2 + 1 + Randomizer.nextInt(nodeCount / 2);
             node = tree.getNode(nodeNr);
 
         } while (node.isRoot() || node.isLeaf());//rule out the leaf and root
@@ -67,155 +65,83 @@ public class TwoNodeOperator extends TreeOperator {
           } else {
               S = P.getChild(0);
           }
-
-
-         //Step 4: Propose new node times and rates
-         /*
-        Case1: P is the root of tree
-               change 4 rates above C1,C2,S and node
-                P
-               /\
-         node /  \
-             /\   \
-            /  \   \
-           C1  C2  S
-         */
-         if (P.isRoot()) {
-              hastingsratio = RateChange(C1,C2,S,node,P);
-         }
+          double ts = S.getHeight();
 
          /*
-        Case2: P is not the root
-               firstly, change the 4 rates above C1, C2, S and node
-               secondly, change the rate above P, i.e. r5 ---> r5'
-
-                 GP
-                 /\
-              P /  \
-               /\   \
-         node /  \   \
-             /\   \   \
-            /  \   \   \
-           C1  C2  S
+         Step 4: Propose new node times
          */
-         else {
-             double t_GP = P.getParent().getHeight();
-             double r5 = branchRateModel.getRateForBranch(P);
-             //original node time of P
-             double t5 = P.getHeight();
-             //propose four rates on branches above C1, C2, S and node
-             double r = RateChange(C1,C2,S,node,P);
-             //get the proposed node time of P
-             double t5_ = P.getHeight();
-             //To Propose a new rate for branch above P
-             double r5_ = r5 * (t_GP - t5) / (t_GP - t5_);
-             rates.setValue(P.getNr(),r5_);
-
-             hastingsratio = r * (t_GP - t5) / (t_GP - t5_);
-
-         }
-
-        return Math.log(hastingsratio);
-    }
-
-
-    /*
-    This method changes four rates on the branches above N1, N2, S, C
-    In the meantime, two node times are proposed for C and P
-                P
-               /\
-            C /  \
-             /\   \
-            /  \   \
-           N1  N2  S
-     */
-    public double RateChange (Node N1, Node N2, Node S, Node C, Node P) {
-        //original node times
-        double t1 = N1.getHeight();
-        double t2 = N2.getHeight();
-        double t3 = S.getHeight();
-        double tc = C.getHeight();
+        double tx = node.getHeight();
         double tp = P.getHeight();
 
-        //original rates
-        double r1 = branchRateModel.getRateForBranch(N1);
-        double r2 = branchRateModel.getRateForBranch(N2);
-        double r3 = branchRateModel.getRateForBranch(S);
-        double r4 = branchRateModel.getRateForBranch(C);
-
-        //propose new node times
-        double T [] = NodeTime(C, P);
-        double tc_ = T[0]; double tp_ = T[1];
-
+        double u1 = Randomizer.uniform(-twindowSize,twindowSize);
+        double u2 = Randomizer.uniform(-twindowSize,twindowSize);
+/*
+        double a = tx + u1;
+        double b = tp + u2;
+        double tx_ = Math.min(a,b);
+        double tp_ = Math.max(a,b);
+*/
+        double tx_ = tx + u1;
+        double tp_ = tp + u2;
         //set the new node times
+        double t1 = C1.getHeight();
+        double t2 = C2.getHeight();
+
+        if (tx_ >= tp_){
+            return Double.NEGATIVE_INFINITY;
+        }
+
         if (P.isRoot()) {
-            if (tc_ < Math.max(t1, t2)) {
+            if (tx_ < Math.max(t1, t2)) {
                 return Double.NEGATIVE_INFINITY;
             } else {
-                C.setHeight(tc_);
+                node.setHeight(tx_);
                 P.setHeight(tp_);
             }
         } else {
-            if (tc_ < Math.max(t1, t2) || tp_ > P.getParent().getHeight()) {
+            if (tx_ < Math.max(t1, t2) || tp_ > P.getParent().getHeight()) {
                 return Double.NEGATIVE_INFINITY;
             } else {
-                C.setHeight(tc_);
+                node.setHeight(tx_);
                 P.setHeight(tp_);
             }
         }
 
+         /*
+         Step 5: Propose new rates
+         */
+        //original rates
+        double r1 = branchRateModel.getRateForBranch(C1);
+        double r2 = branchRateModel.getRateForBranch(C2);
+        double r3 = branchRateModel.getRateForBranch(S);
+        double r4 = branchRateModel.getRateForBranch(node);
         //propose new rates
-        double r1_ = r1 * (tc - t1) / (tc_ - t1);
-        double r2_ = r2 * (tc - t2) / (tc_ - t2);
-        double r3_ = r3 * (tp - t3) / (tp_ - t3);
-        double r4_ = r4 * (tp - tc) / (tp_- tc_);
-
+        double r1_ = r1 * (tx - t1) / (tx_ - t1);
+        double r2_ = r2 * (tx - t2) / (tx_ - t2);
+        double r3_ = r3 * (tp - ts) / (tp_ - ts);
+        double r4_ = r4 * (tp - tx) / (tp_- tx_);
         //update the new rates
-        rates.setValue(N1.getNr(),r1_);
-        rates.setValue(N2.getNr(),r2_);
+        rates.setValue(C1.getNr(),r1_);
+        rates.setValue(C2.getNr(),r2_);
         rates.setValue(S.getNr(),r3_);
-        rates.setValue(C.getNr(),r4_);
+        rates.setValue(node.getNr(),r4_);
 
         //the determinant of the Jacobian matrix
-        double nu = (tc - t1)*(tc - t2)*(tp - t3)*(tp - tc);
-        double de = (tc_ - t1)*(tc_ - t2)*(tp_ - t3)*(tp_ - tc_);
+        double nu = (tx - t1)*(tx - t2)*(tp - ts)*(tp - tx);
+        double de = (tx_ - t1)*(tx_ - t2)*(tp_ - ts)*(tp_ - tx_);
+        double r = nu / de;
 
-        return nu/de;
-    }
-
-    /*
-    This method proposes two node times
-    And also give the new values to the corresponding nodes
-
-            A____________ t_A
-            /\
-        D  /__\__________ t_D
-          /\   \
-         /  \   \
-
-    returns proposed times by Time = [t_D'  t_A']
-     */
-    public double[] NodeTime (Node D, Node A) {
-
-        //generate two random variables from Uniform distribution
-
-        double u1 = Randomizer.uniform(-twindowSize,twindowSize);
-        double u2 = Randomizer.uniform(-twindowSize,twindowSize);
-
-        double a = D.getHeight() + u1;
-        double b = A.getHeight() + u2;
-
-        double t_D = Math.min(a,b);
-        double t_A = Math.max(a,b);
-
-        //set the new node times
-        //D.setHeight(t_D);
-        //A.setHeight(t_A);
-
-        //return the new node times for proposing new rates
-        double Time []= {0.0,0.0};
-        Time[0] =t_D; Time[1] =t_A;
-        return Time;
+        if (!P.isRoot()) {
+            double t_GP = P.getParent().getHeight();
+            double r5 = branchRateModel.getRateForBranch(P);
+            //To Propose a new rate for branch above P
+            double r5_ = r5 * (t_GP - tp) / (t_GP - tp_);
+            rates.setValue(P.getNr(),r5_);
+            hastingsratio = r * (t_GP - tp) / (t_GP - tp_);
+            return Math.log(8*hastingsratio);
+         } else {
+            return Math.log(8*r);
+        }
     }
 
     @Override
