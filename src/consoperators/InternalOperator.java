fe553f312266edd68fa2365384ae34454123514c
rong <rzha419@aucklanduni.ac.nz>
2019-08-27 16:13:19 +1200

    add jacobian

--- src/consoperators/InternalOperator.java
+++ src/consoperators/InternalOperator.java
@@ -13,6 +13,7 @@ import beast.math.distributions.ParametricDistribution;
 import beast.util.Randomizer;
 import org.apache.commons.math.MathException;
 import org.apache.commons.math3.special.Erf;
+import org.apache.commons.math3.util.FastMath;
 import java.text.DecimalFormat;
 import java.util.List;
 
@@ -26,6 +27,7 @@ public class InternalOperator extends TreeOperator {
     final public Input<RealParameter> stdevInput = new Input<>("stdev", "the distribution governing the rates among branches.");
 
 
+
     enum Mode {
         quantiles,
         rates
@@ -35,6 +37,8 @@ public class InternalOperator extends TreeOperator {
     private RealParameter rates;
     private RealParameter quantiles;
     private RealParameter ucldStdev;
+    double new_stdev;
+    double stdev;
 
     protected ParametricDistribution distribution;
 
@@ -48,16 +52,16 @@ public class InternalOperator extends TreeOperator {
         } else {
             rates = rateInput.get();
         }
-        ucldStdev = stdevInput.get();
+        stdev = stdevInput.get().getValue();
     }
 
     @Override
     public double proposal() {
         final Tree tree = treeInput.get(this);
-        double stdev = ucldStdev.getValue();
+        ucldStdev=stdevInput.get();
         double m_fScaleFactor = 0.5;
         double b =  (m_fScaleFactor + (Randomizer.nextDouble() * ((1.0 / m_fScaleFactor) - m_fScaleFactor)));
-        double new_stdev = stdev * b;
+        new_stdev = stdev * b;
         ucldStdev.setValue(new_stdev);
 
         //the original rates
@@ -93,11 +97,11 @@ public class InternalOperator extends TreeOperator {
            double q_node = quantiles.getValues()[node.getNr()];
            double q_j = quantiles.getValues()[son.getNr()];
            double q_k = quantiles.getValues()[daughter.getNr()];
-           System.out.println("q1="+q_node+",q2="+q_j+",q3="+q_k);
+           //System.out.println("q1="+q_node+",q2="+q_j+",q3="+q_k);
            r_node = getRealRate(q_node);
            r_j = getRealRate(q_j);
            r_k = getRealRate(q_k);
-           System.out.println("r1="+r_node+",r2="+r_j+",r3="+r_k);
+           //System.out.println("r1="+r_node+",r2="+r_j+",r3="+r_k);
        } else {
            // use real rates
            r_node = rates.getValues()[node.getNr()];
@@ -146,17 +150,13 @@ public class InternalOperator extends TreeOperator {
        //Step4: calculate the Hastings ratio
        double nu =(upper - t_x) * (t_x - t_j) * (t_x - t_k) ;
        double de = (upper - t_x_) * (t_x_ - t_j) * (t_x_ - t_k);
-       double icdfq = getDicdf(getRateQuantiles(r_node_)) * getDicdf(getRateQuantiles(r_j_)) * getDicdf(getRateQuantiles(r_k_));
-       double JD =  b * nu /de;
+       double JD =  b * getDicdf(r_node) * getDicdf(r_j) * getDicdf(r_k)* nu / de;
 
        return Math.log(JD);
 }
 
     private double getRateQuantiles (double r) {
         try {
-            LogNormalDistributionModel mylognormal=(LogNormalDistributionModel)distribution;
-            double S=mylognormal.SParameterInput.get().getValue();
-            //System.out.println("S="+S);
             return distribution.cumulativeProbability(r);
         } catch (MathException e) {
             throw new RuntimeException("Failed to compute inverse cumulative probability because rate =" + r);
@@ -171,11 +171,17 @@ public class InternalOperator extends TreeOperator {
         }
     }
 
-    private double getDicdf(double q) {
-        LogNormalDistributionModel mylognormal=(LogNormalDistributionModel)distribution;
-        double S=mylognormal.SParameterInput.get().getValue();
-        double miu = Math.log(1/(Math.sqrt(1+S)));
-        return miu + 4 * Math.sqrt(2) * Math.E *((2*q-1)* (Erf.erf(2*q-1)) + (Math.exp(-(2*q-1)*(2*q-1)))/(Math.sqrt(Math.PI)));
+    private double getDicdf(double r) {
+
+        double miu = Math.log(1/(Math.sqrt(1+stdev)));
+        double miu_ = Math.log(1/(Math.sqrt(1+new_stdev)));
+        double jita = Math.sqrt(Math.log(1 + stdev * stdev));
+        double a = 1/(jita * r);
+        double b = 1 - Math.pow((Math.log(r) - miu) / (Math.sqrt(2) * jita),2);
+        double c = erfInv(2 * getRateQuantiles(r) -1);
+        double d = miu_ + Math.sqrt(2)*Math.E*c + c*c + b;
+
+        return a * Math.exp(d);
     }
 
     /*
@@ -228,6 +234,87 @@ public class InternalOperator extends TreeOperator {
             return "Try setting window size to about " + formatter.format(newWindowSize);
         } else return "";
     }
+
+   private static double erfInv(final double x) {
+    // beware that the logarithm argument must be
+    // commputed as (1.0 - x) * (1.0 + x),
+    // it must NOT be simplified as 1.0 - x * x as this
+    // would induce rounding errors near the boundaries +/-1
+             double w = - FastMath.log((1.0 - x) * (1.0 + x));
+             double p;
+
+             if (w < 6.25) {
+                        w -= 3.125;
+                        p =  -3.6444120640178196996e-21;
+                        p =   -1.685059138182016589e-19 + p * w;
+                        p =   1.2858480715256400167e-18 + p * w;
+                        p =    1.115787767802518096e-17 + p * w;
+                        p =   -1.333171662854620906e-16 + p * w;
+                        p =   2.0972767875968561637e-17 + p * w;
+                        p =   6.6376381343583238325e-15 + p * w;
+                        p =  -4.0545662729752068639e-14 + p * w;
+                        p =  -8.1519341976054721522e-14 + p * w;
+                        p =   2.6335093153082322977e-12 + p * w;
+                        p =  -1.2975133253453532498e-11 + p * w;
+                        p =  -5.4154120542946279317e-11 + p * w;
+                        p =    1.051212273321532285e-09 + p * w;
+                        p =  -4.1126339803469836976e-09 + p * w;
+                        p =  -2.9070369957882005086e-08 + p * w;
+                        p =   4.2347877827932403518e-07 + p * w;
+                        p =  -1.3654692000834678645e-06 + p * w;
+                        p =  -1.3882523362786468719e-05 + p * w;
+                        p =    0.0001867342080340571352 + p * w;
+                        p =  -0.00074070253416626697512 + p * w;
+                        p =   -0.0060336708714301490533 + p * w;
+                        p =      0.24015818242558961693 + p * w;
+                        p =       1.6536545626831027356 + p * w;
+                    } else if (w < 16.0) {
+                        w = FastMath.sqrt(w) - 3.25;
+                        p =   2.2137376921775787049e-09;
+                        p =   9.0756561938885390979e-08 + p * w;
+                        p =  -2.7517406297064545428e-07 + p * w;
+                        p =   1.8239629214389227755e-08 + p * w;
+                        p =   1.5027403968909827627e-06 + p * w;
+                        p =   -4.013867526981545969e-06 + p * w;
+                        p =   2.9234449089955446044e-06 + p * w;
+                      p =   1.2475304481671778723e-05 + p * w;
+                        p =  -4.7318229009055733981e-05 + p * w;
+                        p =   6.8284851459573175448e-05 + p * w;
+                        p =   2.4031110387097893999e-05 + p * w;
+                        p =   -0.0003550375203628474796 + p * w;
+                        p =   0.00095328937973738049703 + p * w;
+                        p =   -0.0016882755560235047313 + p * w;
+                        p =    0.0024914420961078508066 + p * w;
+                        p =   -0.0037512085075692412107 + p * w;
+                        p =     0.005370914553590063617 + p * w;
+                        p =       1.0052589676941592334 + p * w;
+                        p =       3.0838856104922207635 + p * w;
+                    } else if (!Double.isInfinite(w)) {
+                        w = FastMath.sqrt(w) - 5.0;
+                        p =  -2.7109920616438573243e-11;
+                        p =  -2.5556418169965252055e-10 + p * w;
+                        p =   1.5076572693500548083e-09 + p * w;
+                        p =  -3.7894654401267369937e-09 + p * w;
+                        p =   7.6157012080783393804e-09 + p * w;
+                        p =  -1.4960026627149240478e-08 + p * w;
+                        p =   2.9147953450901080826e-08 + p * w;
+                        p =  -6.7711997758452339498e-08 + p * w;
+                        p =   2.2900482228026654717e-07 + p * w;
+                        p =  -9.9298272942317002539e-07 + p * w;
+                        p =   4.5260625972231537039e-06 + p * w;
+                        p =  -1.9681778105531670567e-05 + p * w;
+                        p =   7.5995277030017761139e-05 + p * w;
+                        p =  -0.00021503011930044477347 + p * w;
+                       p =  -0.00013871931833623122026 + p * w;
+                    p =       1.0103004648645343977 + p * w;
+                       p =       4.8499064014085844221 + p * w;
+                  } else {
+                   p = Double.POSITIVE_INFINITY;
+                 }
+
+               return p * x;
+
+          }
 
 
 }
