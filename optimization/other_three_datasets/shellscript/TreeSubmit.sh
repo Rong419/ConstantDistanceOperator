ec7b11539431055bc24b2fd436af34fa61ac1b6a
ryanzhang <rzha419@aucklanduni.ac.nz>
2019-09-08 20:23:07 +1200

    scripts for summarise trees using TreeAnnotator

---
+++ optimization/other_three_datasets/shellscript/TreeSubmit.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+TEMPLATE=Treetemplate.sl
+
+for data in {anolis,Shankarappa,RSV2}
+do
+  for param in {1..2}
+  do
+    for sim in {1..3}
+    do
+    sed "s/FILE/${data}_${param}_${sim}/g" ${TEMPLATE} > ./Treerandom.sl 
+    echo "summarise primates${ft}_${sim}.trees"
+    sbatch Treerandom.sl 
+    rm -f Treerandom.sl 
+    sleep 5
+    done
+  done
+done
---
+++ optimization/primates_analysis/sample_ucldmean/shellscript/TreeSubmit.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+TEMPLATE=Treetemplate.sl
+
+for ft in {1..2}
+do  
+for sim in {1..3}
+  do 
+  sed "s/FILE/${ft}_${sim}/g" ${TEMPLATE} > ./Treerandom.sl 
+    echo "summarise primates${ft}_${sim}.trees"
+    sbatch Treerandom.sl 
+    rm -f Treerandom.sl 
+    sleep 5
+ done
+done
