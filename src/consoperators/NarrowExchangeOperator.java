f37cbe402b07347329921344d980793eb8cb5dd9
rong <rzha419@aucklanduni.ac.nz>
2019-09-25 17:23:15 +1200

    start narrow exchange operator

---
+++ src/consoperators/NarrowExchangeOperator.java
@@ -0,0 +1,129 @@
+package consoperators;
+
+import beast.core.Description;
+import beast.core.Input;
+import beast.core.parameter.RealParameter;
+import beast.evolution.operators.TreeOperator;
+import beast.evolution.tree.Node;
+import beast.evolution.tree.Tree;
+import beast.util.Randomizer;
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+import java.util.Arrays;
+
+@Description("Implements the NARROW variety in Exchange operator in beas2" +
+             "but maintain the genetic distances by proposing branch rates")
+public class NarrowExchangeOperator extends TreeOperator {
+    final public Input<RealParameter> rateInput = new Input<>("rates", "the rates associated with nodes in the tree for sampling of individual rates among branches.", Input.Validate.REQUIRED);
+
+    private RealParameter rates;
+
+    @Override
+    public void initAndValidate() {
+        rates = rateInput.get();
+    }
+
+    @Override
+    public double proposal() {
+        final Tree tree = treeInput.get(this);
+
+        //System.out.println("r = "+ Arrays.toString(rates.getValues()));
+        // get the number of internal nodes
+        final int internalNodes = tree.getInternalNodeCount();
+        if (internalNodes <= 1) {
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        // randomly select a valid grandParent node
+        // whose child nodes cannot be both leaf nodes
+        Node grandParent = tree.getNode(internalNodes + 1 + Randomizer.nextInt(internalNodes));
+        while (grandParent.getChild(0).isLeaf() && grandParent.getChild(1).isLeaf()) {
+            grandParent = tree.getNode(internalNodes + 1 + Randomizer.nextInt(internalNodes));
+        }
+
+        // access to the child nodes of grandParent
+        // the older child node is denoted by parent
+        // the younger child nodes is denoted by uncle
+        Node parent = grandParent.getChild(0);
+        Node uncle = grandParent.getChild(1);
+        if (parent.getHeight() < uncle.getHeight()) {
+            parent = grandParent.getChild(1);
+            uncle = grandParent.getChild(0);
+        }
+
+        // randomly select a child node of parent, denoted by child
+        final Node child = (Randomizer.nextBoolean() ? parent.getChild(0) : parent.getChild(1));
+
+        // get the node times
+        double tp = parent.getHeight();
+        double tc = child.getHeight();
+        double tu = uncle.getHeight();
+        double tgp = grandParent.getHeight();
+
+        // avoid the parent is a node with tip date
+        if( parent.isLeaf() ) {
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        // the probability of making this proposal
+        // = the probability of picking the random grandParent
+        // = 1 / (the number of all valid grandParent nodes in the current tree)
+        int validGP = 0;
+
+        for(int i = internalNodes + 1; i < 1 + 2*internalNodes; ++i) {
+            validGP += isg(tree.getNode(i));
+        }
+
+        final int c2 = sisg(parent) + sisg(uncle);
+
+
+
+        // rates in original state
+        double ru = rates.getValue(uncle.getNr());
+        double rc = rates.getValue(child.getNr());
+        double rp = rates.getValue(parent.getNr());
+        //System.out.println("rc="+rc+",ru="+ru+",rp="+rp);
+        // exchange child and uncle, child.e. child <--> uncle
+        exchangeNodes(child, uncle, parent, grandParent);
+
+        // propose  new rates
+        double upper = rc * (tp - tc) / (tgp - tc);
+        double rc_ = Randomizer.uniform(0,upper);
+        //System.out.println("upper="+upper);
+        double ru_ = (ru * (tgp - tu) + rp * (tgp - tp)) / (tp - tu);
+
+        double rp_ = (rc * (tp - tc) - rc_ * (tgp - tc)) / (tgp - tp);
+
+        //System.out.println("rc'="+rc_+",ru'="+ru_+",rp'="+rp_);
+        // set the new rates
+        rates.setValue(child.getNr(),rc_);
+        rates.setValue(uncle.getNr(),ru_);
+        rates.setValue(parent.getNr(),rp_);
+
+        // the probability of going back to the original state
+        // = 1 / (the number of all valid grandParent nodes in the proposed tree)
+        final int validGPafter = validGP - c2 + sisg(parent) + sisg(uncle);
+
+        // return the hastings ratio in log space
+        double R1 = (float)validGP/validGPafter;
+        double R2 = (rc * (tgp- tu) * (tp - tc)) / (ru_ * (tp - tu) * (tgp - tc));
+        //System.out.println("R1="+R1+",R2="+R2);
+        return Math.log(R1 * R2);
+    }
+
+    private int isg(final Node n) {
+        return (n.getLeft().isLeaf() && n.getRight().isLeaf()) ? 0 : 1;
+    }
+
+    private int sisg(final Node n) {
+        return n.isLeaf() ? 0 : isg(n);
+    }
+
+    private void exchangeNodes(Node i, Node j,
+                                 Node p, Node jP) {
+        // precondition p -> i & jP -> j
+        replace(p, i, j);
+        replace(jP, j, i);
+        // postcondition p -> j & p -> i
+    }
+}
---
+++ src/test/NarrowExchangeOperatorTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import consoperators.NarrowExchangeOperator;
+import org.junit.Test;
+
+import beast.core.State;
+import beast.evolution.alignment.Alignment;
+import beast.evolution.alignment.Sequence;
+import beast.util.Randomizer;
+import beast.util.TreeParser;
+import junit.framework.TestCase;
+
+
+public class NarrowExchangeOperatorTest extends TestCase {
+
+    @Test
+    public void test4Taxa() throws Exception {
+
+        int runs = 1;
+        Randomizer.setSeed(666);
+        // test that going from source tree to target tree
+        // is as likely as going the other way around
+        // taking the HR in account.
+        Sequence A = new Sequence("A", "A");
+        Sequence B = new Sequence("B", "A");
+        Sequence C = new Sequence("C", "A");
+        Sequence D = new Sequence("D", "A");
+
+        Alignment data = new Alignment();
+        data.initByName("sequence", A, "sequence", B, "sequence", C, "sequence", D,
+                "dataType", "nucleotide"
+        );
+        String sourceTree = "((A:2.0,B:2.0):1.0,(C:1.0,D:1.0):2.0):0.0"; // ((A,B),(C,D))
+        testNarrowExchange(sourceTree,runs, data);
+    }
+
+    void testNarrowExchange(String sourceTree, int runs, Alignment data) throws Exception {
+
+        // first test going from source to target
+        double match = 0;
+        for (int i = 0; i < runs; i++) {
+            TreeParser tree = new TreeParser();
+            tree.initByName("taxa", data, "newick", sourceTree, "IsLabelledNewick", true);
+            State state = new State();
+            state.initByName("stateNode", tree);
+            state.initialise();
+            NarrowExchangeOperator operator = new NarrowExchangeOperator();
+            operator.initByName("isNarrow", true, "tree", tree, "weight", 1.0);
+
+            double logHR = operator.proposal();
+            String treeString = tree.getRoot().toNewick();
+        }
+    }
+
+}
