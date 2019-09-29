7aea3e3084e42b9fa23525944702b151888bb2ff
rong <rzha419@aucklanduni.ac.nz>
2019-09-30 09:34:35 +1300

    validated narrow exchange operator that maintain the distances within the group

--- src/consoperators/NarrowExchangeOperator.java
+++ src/consoperators/NarrowExchangeOperator.java
@@ -8,39 +8,42 @@ import beast.evolution.tree.Node;
 import beast.evolution.tree.Tree;
 import beast.util.Randomizer;
 import java.text.DecimalFormat;
-import java.util.Arrays;
+
 
 @Description("Implements the NARROW variety in Exchange operator in beas2" +
              "but maintain the genetic distances by proposing branch rates")
 public class NarrowExchangeOperator extends TreeOperator {
-    final public Input<RealParameter> rateInput = new Input<>("rates", "the rates associated with nodes in the tree for sampling of individual rates among branches.", Input.Validate.REQUIRED);
+    public final  Input<RealParameter> rateInput = new Input<>("rates", "the rates associated with nodes in the tree for sampling of individual rates among branches.", Input.Validate.REQUIRED);
+    public final Input<Double> rwindowSizeInput =
+            new Input<>("rwindowSize", "the size of the window when proposing branch rate for node 'parent'", Input.Validate.REQUIRED);
 
     private RealParameter rates;
+    private double rwindowSize;
 
     @Override
     public void initAndValidate() {
         rates = rateInput.get();
+        rwindowSize = rwindowSizeInput.get();
     }
 
     @Override
     public double proposal() {
         final Tree tree = treeInput.get(this);
 
-
+        // Step 1: randomly select a valid grandParent node
         // get the number of internal nodes
         final int internalNodes = tree.getInternalNodeCount();
         if (internalNodes <= 1) {
             return Double.NEGATIVE_INFINITY;
         }
 
-        // randomly select a valid grandParent node
-        // whose child nodes cannot be both leaf nodes
+        // child nodes of a valid grandParent node cannot be both leaf nodes
         Node grandParent = tree.getNode(internalNodes + 1 + Randomizer.nextInt(internalNodes));
         while (grandParent.getChild(0).isLeaf() && grandParent.getChild(1).isLeaf()) {
             grandParent = tree.getNode(internalNodes + 1 + Randomizer.nextInt(internalNodes));
         }
 
-        // access to the child nodes of grandParent
+        // Step2: access to the child nodes of grandParent
         // the older child node is denoted by parent
         // the younger child nodes is denoted by uncle
         Node parent = grandParent.getChild(0);
@@ -77,18 +80,16 @@ public class NarrowExchangeOperator extends TreeOperator {
 
 
         // rates in original state
-        Double [] r = rates.getValues();
         double ru = rates.getValue(uncle.getNr());
         double rc = rates.getValue(child.getNr());
         double rp = rates.getValue(parent.getNr());
 
-        // exchange child and uncle, child.e. child <--> uncle
+        // Step3: exchange child and uncle, child.e. child <--> uncle
         exchangeNodes(child, uncle, parent, grandParent);
 
-        // propose new rates
+        // Step4: propose new rates
         double upper = rc * (tp - tc) / (tgp - tp);
-        double rp_ = Randomizer.uniform(0,upper);
-
+        double rp_ = Randomizer.uniform(-rwindowSize,rwindowSize) + rp;
 
         if (rp_ <= 0 || rp_ >= upper) {
             return Double.NEGATIVE_INFINITY;
@@ -104,25 +105,21 @@ public class NarrowExchangeOperator extends TreeOperator {
         rates.setValue(uncle.getNr(),ru_);
         rates.setValue(parent.getNr(),rp_);
 
+
+
+        // Step4: return the hastings ratio in log space
         // the probability of going back to the original state
         // = 1 / (the number of all valid grandParent nodes in the proposed tree)
         final int validGPafter = validGP - c2 + sisg(parent) + sisg(uncle);
 
-        // return the hastings ratio in log space
-        double R1 = (float)validGP/validGPafter;
-        double R2 = (rc * rp * (tp - tc)) / (rp_ * ru_ * (tp - tu));
-        //double R3 = Math.log(1/scale);
-        /*
-        System.out.println("r = "+ Arrays.toString(r));
-        System.out.println("rc="+rc+",ru="+ru+",rp="+rp);
-        System.out.println("upper="+upper+",rp_="+rp_);
-        System.out.println("rc'="+rc_+",ru'="+ru_+",rp'="+rp_);
-        System.out.println("r'=" + Arrays.toString(rates.getValues()));
-        System.out.println("R1="+R1+",R2="+R2);
-        */
+        // the ratio of exchange child and uncle
+        double hastingsRatio = (float)validGP/validGPafter;
 
-        double J = ((tp - tc) * (tgp - tu)) / ((tgp - tc) * (tp - tu));
-        return Math.log(R1 * R2);
+        // the determinant of jacobian matrix
+        double Jacobian = ((tp - tc) * (tgp - tu)) / ((tgp - tc) * (tp - tu));
+
+        // the Green's corrected hastings ratio
+        return Math.log(hastingsRatio * Jacobian);
     }
 
     private int isg(final Node n) {
@@ -141,5 +138,55 @@ public class NarrowExchangeOperator extends TreeOperator {
         // postcondition p -> j & p -> i
     }
 
+    /*
+     Tuning the parameter: rwindowsize
+    */
+    @Override
+    public double getCoercableParameterValue() {
+        return rwindowSize;
+    }
+
+
+    @Override
+    public void setCoercableParameterValue(double value) {
+        rwindowSize = value;
+    }
+
+    /**
+     * called after every invocation of this operator to see whether
+     * a parameter can be optimised for better acceptance hence faster
+     * mixing
+     *
+     * @param logAlpha difference in posterior between previous state & proposed state + hasting ratio
+     */
+
+    @Override
+    public void optimize(double logAlpha) {
+        // must be overridden by operator implementation to have an effect
+        double delta = calcDelta(logAlpha);
+
+        delta += Math.log(rwindowSize);
+        rwindowSize = Math.exp(delta);
+    }
+
+    @Override
+    public final String getPerformanceSuggestion() {
+        double prob = m_nNrAccepted / (m_nNrAccepted + m_nNrRejected + 0.0);
+        double targetProb = getTargetAcceptanceProbability();
+
+        double ratio = prob / targetProb;
+        if (ratio > 2.0) ratio = 2.0;
+        if (ratio < 0.5) ratio = 0.5;
+
+        // new scale factor
+        double newWindowSize = rwindowSize * ratio;
+
+        DecimalFormat formatter = new DecimalFormat("#.###");
+        if (prob < 0.10) {
+            return "Try setting window size to about " + formatter.format(newWindowSize);
+        } else if (prob > 0.40) {
+            return "Try setting window size to about " + formatter.format(newWindowSize);
+        } else return "";
+    }
 
 }
