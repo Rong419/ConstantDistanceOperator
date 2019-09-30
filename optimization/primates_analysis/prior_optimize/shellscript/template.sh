707589dfd097c8d3ab9aec700b03ebd405009b5a
rong <rzha419@aucklanduni.ac.nz>
2019-09-30 15:38:39 +1300

    templates for coupled MCMC

--- optimization/primates_analysis/prior_optimize/shellscript/template.sh
+++ optimization/primates_analysis/prior_optimize/shellscript/template.sh
@@ -6,11 +6,11 @@
 #SBATCH --cpus-per-task=8       # OpenMP Threads
 #SBATCH --ntasks=1              # not use MPI
 #SBATCH --hint=multithread      # A multithreaded job, also a Shared-Memory Processing (SMP) job
-#SBATCH -D /nesi/project/nesi00390/rong/operator/primates/samplemean/output    # The initial directory
+#SBATCH -D /nesi/project/nesi00390/rong/operator/primates/output    # The initial directory
 #SBATCH -o O_Primates_PARAM_SIM.txt         # The output file
 #SBATCH -e E_Primates_PARAM_SIM.txt         # The error file
 
 module load beagle-lib/3.0.1-gimkl-2017a
 module load Java/1.8.0_144
 
-srun java -Xmx2800m -Djava.library.path=$BEAGLE_LIB_PATH -jar /nesi/project/nesi00390/rong/operator/ConstantDistanceOperator.jar -threads 8 -beagle_SSE /nesi/project/nesi00390/rong/operator/primates/samplemean/xml/primates_PARAM_SIM.xml
+srun java -Xmx2800m -Djava.library.path=$BEAGLE_LIB_PATH -jar /nesi/project/nesi00390/rong/operator/ConstantDistanceOperator.jar -threads 8 -beagle_SSE /nesi/project/nesi00390/rong/operator/primates/xml/primates_PARAM_SIM.xml
