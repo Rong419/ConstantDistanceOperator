01aa94ade2416d8d0db3fae2bc688f81d7ebb493
ryanzhang <rzha419@aucklanduni.ac.nz>
2019-10-14 22:45:47 +1300

    shell script for running loganalyser

--- validation/efficiency/getESS.sh
+++ validation/efficiency/getESS.sh
@@ -2,11 +2,19 @@
 
 for data in {anolis,Shankarappa,RSV2}
 do
-
 	for model in {Cons,Category}
 	do
-	/Users/rzha419/Applications/BEAST2.6.0/bin/loganalyser -oneline /Users/rzha419/Desktop/validation/efficiency/others/${data}${model}/logs/*.log >/Users/rzha419/Desktop/validation/efficiency/others/ess/ESS_${data}${model}.txt
-	done
-	
+	/Applications/BEAST2.6.0/bin/loganalyser -oneline ~/Desktop/validation/efficiency/others/${data}${model}/logs/*.log >~/Desktop/validation/efficiency/others/ess/ESS_${data}${model}.txt
+	done	
 done
+
+
+
+for model in {Cons,Category}
+do
+	/Applications/BEAST2.6.0/bin/loganalyser -oneline ~/Desktop/validation/efficiency/simulated/20taxa/Short${model}/logs/*.log >~/Desktop/validation/efficiency/simulated/20taxa/ess/ESS_Short${model}20taxa.txt
+	/Applications/BEAST2.6.0/bin/loganalyser -burnin 25 -oneline ~/Desktop/validation/efficiency/simulated/20taxa/Medium${model}/logs/*.log >~/Desktop/validation/efficiency/simulated/20taxa/ess/ESS_Medium${model}20taxa.txt
+done
+
+
 
