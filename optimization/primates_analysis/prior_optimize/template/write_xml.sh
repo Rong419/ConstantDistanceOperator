4049b6798196fc60a9e8166464ac063a28a752ce
rong <rzha419@aucklanduni.ac.nz>
2019-10-17 11:35:50 +1300

    use 87 taxa in templates

--- optimization/primates_analysis/prior_optimize/template/write_xml.sh
+++ optimization/primates_analysis/prior_optimize/template/write_xml.sh
@@ -2,7 +2,7 @@
  
 for param in {1..2}
 do 
-    for sim in {1..3}
+    for sim in {4..6}
     do
     sed "s/FILE/${param}_${sim}/g" ./primatesMC3_${param}.xml > ./xml/primatesMC3_${param}_${sim}.xml
     echo "use primates_${param}.xml to write primates_${param}_${sim}.xml"
